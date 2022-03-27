443b487c388a449df5a62259d50cc5b08ef853e5
DHawk98 <dominik.jastrzab@gmail.com>
2022-03-27 11:57:41 +0200

    Working relationships

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -10,13 +10,6 @@ var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Nort
 var conn = new SqlConnection(connectionString);
 
 
-var result = conn.Query<Region>("SELECT * FROM Region");
-
-foreach (var item in result)
-{
-    Console.WriteLine($"{item.RegionId}: {item.RegionDescription}");
-}
-
 var joinResult = conn.Query<Product, Category, Product>("SELECT * FROM Products p " +
     "JOIN Categories c on p.CategoryID = c.CategoryID",
     (product, category) =>
@@ -24,7 +17,8 @@ var joinResult = conn.Query<Product, Category, Product>("SELECT * FROM Products 
         product.Category = category;
         return product;
 
-    });
+    },
+    splitOn:"CategoryId");
 
 foreach (var item in joinResult)
 {
